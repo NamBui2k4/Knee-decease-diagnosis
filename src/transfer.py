c29764b6b4e11022187e64501838bc51bdbc41fd
Nam <thecallingspectre@gmail.com>
2025-02-11 09:23:25 +0700

    update project

--- src/transfer.py
+++ src/transfer.py
@@ -2,13 +2,14 @@ import os.path
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from sklearn.preprocessing import LabelBinarizer
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.datasets import ImageFolder
 from tqdm import tqdm
 from torch.utils.data import DataLoader
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
 import numpy as np
-from torchvision.models import resnet18, ResNet18_Weights
+from torchvision.models import resnet18, ResNet18_Weights, vgg11 , VGG11_Weights, resnet50, ResNet50_Weights
 from torchvision.transforms import (Compose,
                                     RandomResizedCrop,
                                     RandomHorizontalFlip,
@@ -17,21 +18,19 @@ from torchvision.transforms import (Compose,
                                     Resize,
                                     CenterCrop
                                     )
+from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 import argparse
 import shutil
 import warnings
-
-from torchvision.models import efficientnet_b2
-import torch.nn as nn
-import warnings
+import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore")
 
 
 def parse_arg():
     parse = argparse.ArgumentParser(description='Transfer learning')
-    parse.add_argument('--epochs', '-e', type=int, default=100, help='number of time model training')
+    parse.add_argument('--epochs', '-e', type=int, default=10, help='number of time model training')
     parse.add_argument('--checkpoint-dir', '-d', type=str, default='../checkpoint', help='where to place checkpoint')
     parse.add_argument('--tensorboard', '-b', type=str, default='../dashboad', help='place to store the visualization')
     parse.add_argument('--lr', '-l', type=float, default=1e-3)
@@ -52,6 +51,41 @@ def get_mean_std(loader):
     mean /= len(loader.dataset)
     std /= len(loader.dataset)
     return mean.tolist(), std.tolist()
+
+
+def plot_roc_curve(writer, y_true, y_score, class_names, epoch):
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    colours = ['blue', 'red', 'green', 'purple', 'orange', 'yellow', 'pink']
+    # Chạy vòng lặp qua từng class và tính ROC cho từng class
+    for i, class_name in enumerate(class_names):
+        # Tạo nhãn nhị phân cho class hiện tại: 1 là class i, 0 là các class khác
+        binary_labels = [1 if label == i else 0 for label in y_true]
+
+        # Chuyển dự đoán thành xác suất hoặc giá trị dự đoán tương ứng cho class i
+        binary_scores = [1 if score == i else 0 for score in y_score]
+
+        # Tính ROC cho class i
+        fpr[i], tpr[i], _ = roc_curve(binary_labels, binary_scores)
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Plot ROC curve cho từng class
+    plt.figure(figsize=(10, 8))
+    for i, color in zip(range(len(class_names)), colours):
+        plt.plot(fpr[i], tpr[i], color=color, lw=2,
+                 label=f'ROC curve of class {class_names[i]} (area = {roc_auc[i]:.2f})')
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    writer.add_figure('roc_curve', plt.gcf(), epoch)
+    plt.show()
+    plt.close()
 
 
 def plot_confusion_matrix(writer, cm, class_names, epoch):
@@ -88,6 +122,30 @@ def plot_confusion_matrix(writer, cm, class_names, epoch):
     plt.xlabel('Predicted label')
     writer.add_figure('confusion_matrix', figure, epoch)
 
+def metrics_evaluations(writer, optimizer, epoch, score, metrics, name, model,
+                        model_checkpoint_path):
+    writer.add_scalar(tag=f'{name}/val', scalar_value=score, global_step=epoch)
+
+    # show kết quả
+
+    checkpoint = {
+        'state': model.state_dict(),
+        'epoch': epoch,
+        'optimizer': optimizer.state_dict(),
+        'best_epoch': metrics['best_epoch'],
+        f'best_{name}': metrics[f'best_{name}']
+    }    
+    
+    print('current best', metrics[f'best_{name}'])
+    print('score', score)
+    
+    if metrics[f'best_{name}'] < score:
+        metrics[f'best_{name}'] = score
+        metrics[f'best_epoch'] = epoch
+        torch.save(checkpoint, model_checkpoint_path + '/' + f'best_{name}.pt')
+
+
+
 
 def preprocessing():
     data_transforms = {
@@ -105,8 +163,8 @@ def preprocessing():
         ]),
     }
 
-    temp_val = '/kaggle/input/data-knee-original/dataset/test'
-    temp_train = '/kaggle/input/data-knee-original/dataset/train'
+    # temp_val = '/kaggle/input/knee-dataset/dataset/test'
+    # temp_train = '/kaggle/input/knee-dataset/dataset/train'
 
     train_set = ImageFolder(root='../dataset/train', transform=data_transforms['train'])
     train_loader = DataLoader(
@@ -128,32 +186,37 @@ def preprocessing():
     return train_loader, val_loader
 
 
-def transfer_learning(model, model_name, criterion, optimizer, parse, train_loader, val_loader):
-    writer = SummaryWriter(parse.tensorboard)
+def transfer_learning(model, model_name, classes, criterion, optimizer, parse, train_loader, val_loader):
+    tensorboard_path = parse.tensorboard + '/' + model_name
+    os.makedirs(tensorboard_path, exist_ok=True)
+    
+    writer = SummaryWriter(tensorboard_path)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model.to(device)
 
-    # '/kaggle/working/' + parse.checkpoint_dir + "/" + model_name
-    model_checkpoint_path = '/kaggle/working/' + parse.checkpoint_dir + "/" + model_name
+    # create model checkpoint path is not exist
 
+    model_checkpoint_path = os.path.join(parse.checkpoint_dir, model_name)
     os.makedirs(model_checkpoint_path, exist_ok=True)
-
-    if not os.path.exists(model_checkpoint_path):
-        os.mkdir(model_checkpoint_path)
-
+    
+    metrics = {
+                'best_accuracy' : -999,
+                'best_f1' : -999,
+                'best_precision' : -999,
+                'best_recall' : -999,
+                'best_epoch' : 0
+    }
+    
     for epoch in range(parse.epochs):
         print('-' * 10)
         print(f'Epoch {epoch + 1}/{parse.epochs}')
 
-        best_acc = -999
-        best_epoch = 0
-
         # training
         model.train()
         loss_recorded = []
-        all_classes = []
+        all_pred = []
         all_labels = []
 
         progress_bar = tqdm(train_loader, colour='yellow')
@@ -163,7 +226,7 @@ def transfer_learning(model, model_name, criterion, optimizer, parse, train_load
 
             # foward
             pred = model(images)
-            classes = torch.argmax(pred, dim=1)
+            max_pred = torch.argmax(pred, dim=1)
             loss = criterion(pred, labels)
 
             # backward
@@ -172,7 +235,7 @@ def transfer_learning(model, model_name, criterion, optimizer, parse, train_load
             optimizer.step()  # cap nhat trong so
 
             all_labels.extend(labels.tolist())
-            all_classes.extend(classes.tolist())
+            all_pred.extend(max_pred.tolist())
 
             loss_recorded.append(loss.item())
 
@@ -180,12 +243,17 @@ def transfer_learning(model, model_name, criterion, optimizer, parse, train_load
             writer.add_scalar(tag='Train/Loss', scalar_value=np.mean(loss_recorded),
                               global_step=epoch * len(train_loader) + iter)
 
-        acc = accuracy_score(all_labels, all_classes)
-        print(f'Acc: {acc}, Loss: {np.mean(loss_recorded)}')
+        # numerical metrics
+        acc = accuracy_score(all_labels, all_pred)
+        pre = precision_score(all_labels, all_pred, average='micro')
+        rec = recall_score(all_labels, all_pred, average='micro')
+        f1 = f1_score(all_labels, all_pred,average='micro')
+
+        print(f'Acc: {acc:.4f}, precision: {pre:.4f}, recall: {rec:.4f}, f1_score: {f1:.4f} avg_oss: {np.mean(loss_recorded):.4f}')
 
         model.eval()
         all_labels = []  # Danh sách nhãn ban đầu của data
-        all_outputs = []  # Danh sách output mà model dự đoán
+        all_preds = []
         all_loss = []  # Danh sách loss ghi nhận khi validation
 
         progress_bar = tqdm(val_loader, colour='yellow')
@@ -206,54 +274,93 @@ def transfer_learning(model, model_name, criterion, optimizer, parse, train_load
                 # Ghi nhận kết quả
                 all_loss.append(loss.item())
                 all_labels.extend(labels.tolist())
-                all_outputs.extend(prediction.tolist())
+                all_preds.extend(prediction.tolist())
 
                 progress_bar.set_description(f'Validation')
 
-            # Đánh giá tren tap validation
-            acc = accuracy_score(all_labels, all_outputs)
+            # numerical etrics
+            acc = accuracy_score(all_labels, all_preds)
+            pre = precision_score(all_labels, all_preds, average='micro')
+            rec = recall_score(all_labels, all_preds, average='micro')
+            f1 = f1_score(all_labels, all_preds,average='micro')
             avg_loss = np.mean(all_loss)
-            cm = confusion_matrix(all_labels, all_outputs)
 
-            plot_confusion_matrix(writer, cm, class_names=classes, epoch=epoch)
-
+            print(f'Acc: {acc:.4f}, precision: {pre:.4f}, recall: {rec:.4f}, f1_score: {f1:.4f} avg_oss: {avg_loss:.4f}')
             writer.add_scalar(tag='avg_loss/val', scalar_value=avg_loss, global_step=epoch)
-            writer.add_scalar(tag='acc/val', scalar_value=acc, global_step=epoch)
 
-            # show kết quả
-            print(f'Acc: {acc:.4f}, avg loss: {avg_loss:.4f}')
+            # metrics evaluations
+            metrics_evaluations(writer, optimizer, epoch, acc, metrics, 'accuracy', model,
+                                model_checkpoint_path)
+            metrics_evaluations(writer, optimizer, epoch, f1, metrics, 'f1', model, model_checkpoint_path)
 
-            checkpoint = {
-                'state': model.state_dict(),
-                'epoch': epoch,
-                'optimizer': optimizer.state_dict(),
-                'best_epoch': best_epoch,
-                'best_accuracy': best_acc
-            }
+            
+            metrics_evaluations(writer, optimizer, epoch, pre, metrics, 'precision', model,
+                                model_checkpoint_path)
+            metrics_evaluations(writer, optimizer, epoch, rec, metrics, 'recall', model,
+                                model_checkpoint_path)
 
-            if best_acc < acc:
-                best_acc = acc
-                best_epoc = epoch
-                torch.save(checkpoint, model_checkpoint_path + '/' + 'best.pt')
+            # confusion matrix
+            cm = confusion_matrix(all_labels, all_preds)
+            plot_confusion_matrix(writer, cm, classes, epoch)
+
+            # ROC curve
+            plot_roc_curve(writer, all_labels, all_preds, classes, epoch)
 
 
 if __name__ == '__main__':
     train_loader, val_loader = preprocessing()
     parse = parse_arg()
-    classes = lasses = ['Không mắc bệnh (normal)', 'có dấu hiệu thoái hóa (doubtful)', 'thoái hóa nhẹ (mild)',
-                        'thoái hóa vừa phải (moderate)', 'thoái hóa nghiêm trọng (severe)',
-                        'thiếu xương (osteopenia)', 'loãng xương (Osteoporosis)']
+    classes = ['Không mắc bệnh (normal)', 'có dấu hiệu thoái hóa (doubtful)',
+                'thoái hóa nhẹ (mild)', 'thoái hóa vừa phải (moderate)',
+                'thoái hóa nghiêm trọng (severe)',
+                'thiếu xương (osteopenia)', 'loãng xương (Osteoporosis)']
+
     if os.path.exists(parse.tensorboard):
         shutil.rmtree(parse.tensorboard)
     os.mkdir(parse.tensorboard)
 
-    
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
-
-    model.classifier[1] = nn.Linear(model.last_channel, out_features=7, bias=True)
-
-    optimizer = optim.Adam(model.parameters(), lr=parse.lr)
     # criterion
     criterion = nn.CrossEntropyLoss()
     
-    transfer_learning(model, 'mobilenet_v2', criterion, optimizer, parse, train_loader, val_loader)
+    
+    # --------------------------------- ResNet18 ---------------------------------
+    resnet_18 = resnet18(weights=ResNet18_Weights)
+    resnet_18.fc = nn.Linear(in_features=512, out_features=len(classes), bias=True)
+    optimizer = optim.Adam(resnet_18.parameters(), lr=parse.lr)
+    transfer_learning(resnet_18,
+                        'resnet_18',
+                        classes,
+                        criterion,
+                        optimizer,
+                        parse,
+                        train_loader,
+                        val_loader
+                        )
+    
+#    # --------------------------------- VGG11 ---------------------------------
+#     vgg_11 = vgg11(weights=VGG11_Weights)
+#     vgg_11.classifier[6] = nn.Linear(in_features=4096, out_features=len(classes), bias=True)
+#     optimizer = optim.Adam(vgg_11.parameters(), lr=parse.lr)
+#     transfer_learning(resnet_18,
+#                         'vgg11',
+#                         classes,
+#                         criterion,
+#                         optimizer,
+#                         parse,
+#                         train_loader,
+#                         val_loader
+#                         ) 
+#     # --------------------------------- mobilenet_v2 ---------------------------------
+    
+#     mobilenet = mobilenet_v2(weights=MobileNetV2_Weights)
+#     mobilenet.classifier[1] = nn.Linear(in_features=1280, out_features=len(classes), bias=True)
+#     optimizer = optim.Adam(vgg_11.parameters(), lr=parse.lr)
+#     transfer_learning(mobilenet,
+#                         'mobilenet_v2',
+#                         classes,
+#                         criterion,
+#                         optimizer,
+#                         parse,
+#                         train_loader,
+#                         val_loader
+#                         ) 
